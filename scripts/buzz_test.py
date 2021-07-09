7503bfe9b3a77e5043cc7e1a55a1603be1f83783
Suhan Park <psh117@snu.ac.kr>
2021-07-09 15:14:06 +0900

    update haptics

--- scripts/buzz_test.py
+++ scripts/buzz_test.py
@@ -5,6 +5,7 @@ import os, sys
 import copy
 import numpy as np
 import keyboard
+from std_msgs.msg import Header
 from sensor_msgs.msg import JointState
 import actionlib
 from control_msgs.msg import JointTrajectoryActionGoal, JointTrajectoryAction
@@ -18,16 +19,25 @@ client.wait_for_server()
 # goal = JointTrajectoryActionGoal()
 # goal.header.stamp = rospy.Time.now()
 # # goal.
-msg = JointTrajectory()
-msg.joint_names = ['thumb_cmc', 'index_mcp', 'middle_mcp', 'ring_mcp', 'pinky_mcp']
-point = JointTrajectoryPoint()
-point.positions = [20.0, 20.0, 20.0, 20.0, 20.0]
-point.velocities = [20.0, 20.0, 20.0, 20.0, 20.0]
-point.effort = [20.0, 20.0, 20.0, 20.0, 20.0]
-point.time_from_start = 5.0
-msg.points.append(point)
+# msg = JointTrajectory()
+# msg.joint_names = ['thumb_cmc', 'index_mcp', 'middle_mcp', 'ring_mcp', 'pinky_mcp']
+# point = JointTrajectoryPoint()
+# point.positions = [20.0, 20.0, 20.0, 20.0, 20.0]
+# point.velocities = [20.0, 20.0, 20.0, 20.0, 20.0]
+# point.effort = [20.0, 20.0, 20.0, 20.0, 20.0]
+# point.time_from_start = 5.0
+# msg.points.append(point)
+hap_cmd = JointTrajectory()
+hap_cmd.header = Header()
+hap_cmd.header.stamp = rospy.Time.now()
+hap_cmd.joint_names = ['thumb_brake', 'index_brake', 'middle_brake', 'ring_brake', 
+'pinky_brake', 'thumb_cmc', 'index_mcp', 'middle_mcp', 'ring_mcp', 'pinky_mcp']
 
-rate = rospy.Rate(5)
+point = JointTrajectoryPoint()
+point.positions = [100, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # what you will!
+point.time_from_start = rospy.Duration.from_sec(0.001)
+hap_cmd.points.append(point)
+rate = rospy.Rate(1)
 while rospy.is_shutdown() is False:
-    pub.publish(msg)
+    pub.publish(hap_cmd)
     rate.sleep()
