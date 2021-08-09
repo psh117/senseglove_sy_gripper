d950de063c0af654ff380a183f887bf8c6455a20
Suhan Park <psh117@snu.ac.kr>
2021-08-09 14:52:57 +0900

    add supports for dual hands calibration

--- scripts/glove_gripper_caliration.py
+++ scripts/glove_gripper_caliration.py
@@ -4,16 +4,20 @@ import rospy
 import os, sys
 import copy
 import numpy as np
-import keyboard
+# import keyboard
+from functools import partial
 from sensor_msgs.msg import JointState
 
 class GloveCalibration:
     def __init__(self):
-        self.past_glove_joints = np.zeros(4)
-        self.current_dxl_joints = np.zeros(4)
+        num_joints = 4
+        self.past_glove_joints = {'left':np.zeros(num_joints), 'right':np.zeros(num_joints)}
+        self.current_dxl_joints = {'left':np.zeros(num_joints), 'right':np.zeros(num_joints)}
 
-        self.joint_captures = list()
-        self.capture_trigger = False
+        self.joint_captures = {'left':list(), 'right':list()}
+        self.capture_trigger = {'left':False, 'right':False}
+        self.current_glove_joint = {'left':np.zeros(num_joints),'right':np.zeros(num_joints)}
+        self.filtered_glove_joint = {'left':np.zeros(num_joints),'right':np.zeros(num_joints)}
         self.num_captures = 20
         self.tau = 0.6
 
@@ -23,38 +27,40 @@ class GloveCalibration:
                             'thumb_finger1',
                             'thumb_finger2',
                             'lateral_pinch']
-        rospy.Subscriber("/senseglove/0/rh/joint_states", JointState, self.joint_callback, queue_size=1)
 
-    def joint_callback(self, data):
+        rospy.Subscriber("/senseglove/0/lh/joint_states", JointState, partial(self.joint_callbac,'left'), queue_size=1)
+        rospy.Subscriber("/senseglove/0/rh/joint_states", JointState, partial(self.joint_callbac,'right'), queue_size=1)
+
+    def joint_callback(self, data, location):
         input_pose = data.position
-        self.current_glove_joint = np.array([input_pose[16], input_pose[18], input_pose[2], input_pose[6]])
-        self.filtered_glove_joint = self.current_glove_joint * self.tau + self.past_glove_joints * (1 - self.tau)
-        self.past_glove_joints = self.filtered_glove_joint
+        self.current_glove_joint[location] = np.array([input_pose[16], input_pose[18], input_pose[2], input_pose[6]])
+        self.filtered_glove_joint[location] = self.current_glove_joint[location] * self.tau + self.past_glove_joints[location] * (1 - self.tau)
+        self.past_glove_joints[location] = self.filtered_glove_joint[location]
 
-        if self.capture_trigger:
-            self.joint_captures.append(self.filtered_glove_joint)
+        if self.capture_trigger[location]:
+            self.joint_captures[location].append(self.filtered_glove_joint[location])
             
-            print ('captured {0} samples'.format(len(self.joint_captures)))
+            print ('captured {0} samples'.format(len(self.joint_captures[location])))
 
-            if len(self.joint_captures) >= self.num_captures:
-                self.capture_trigger = False
+            if len(self.joint_captures[location]) >= self.num_captures:
+                self.capture_trigger[location] = False
 
-    def calibration(self):
+    def calibration(self, location):
+        print('-------- Calibration starts [{0}] --------'.format(location))
         for calib_type in self.calib_types:
-            print ('calibrating...', calib_type)
+            print ('calibrating... {0} - {1}'.format(location, calib_type))
+            input('Press enter to start this calibration')
 
-            self.joint_captures = []
+            self.joint_captures[location] = []
+            self.capture_trigger[location] = True
+
+            print('capturing {0} samples! don\'t move'.format(self.num_captures))
+            
+            # wait for capturing
             while rospy.is_shutdown() is False:
-                if keyboard.is_pressed('c'):  # if key 'c' is pressed 
-                    self.capture_trigger = True
-                    print('capturing {0} samples! don\'t move'.format(self.num_captures))
-                    
-                    # wait for capturing
-                    while rospy.is_shutdown() is False:
-                        if self.capture_trigger is False:
-                            break
-                    
+                if self.capture_trigger is False:
                     break
+            
             sum_joints = np.zeros(4)    
             for joints in self.joint_captures:
                 sum_joints += joints
@@ -62,12 +68,13 @@ class GloveCalibration:
             mean_joints = sum_joints / float(self.num_captures)
             print(mean_joints)
 
-            rospy.set_param('/dyros_glove/calibration/' + calib_type, mean_joints.tolist())
+            rospy.set_param('/dyros_glove/calibration/{0}/{1}'.format(location,calib_type), mean_joints.tolist())
 
-        print('calibration done')
+        print('-------- Calibration done [{0}]--------'.format(location))
 
 
 if __name__== '__main__':
     rospy.init_node('glove_gripper_calibration')
     gc = GloveCalibration()
-    gc.calibration()
+    gc.calibration('left')
+    gc.calibration('right')
